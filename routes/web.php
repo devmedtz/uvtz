0f7b0b7ccc831ef9a27fc1ad0d7a3f7360211323
Douglas Gasper <dee@Ds-MacBook-Pro.local>
2022-11-17 17:04:58 +0300

    Changers in permission

--- routes/web.php
+++ routes/web.php
@@ -42,28 +42,28 @@ Route::get('/', function () {
 });
 Route::group(['middleware' => ['auth']], function () {
     //Inventory
-    Route::get('inventory/category', InventoryCategory::class)->middleware(['role:Admin|Accountant|manager'])->name('inventory.category');
-    Route::get('inventory/products', InventoryProduct::class)->middleware(['role:Admin|Accountant|manager'])->name('inventory.product');
-    Route::get('inventory/details/{product_id?}', InventoryDetails::class)->middleware(['role:Admin|Accountant|manager'])->name('inventory.details');
+    Route::get('inventory/category', InventoryCategory::class)->middleware(['role:Admin|Accountant|Manager'])->name('inventory.category');
+    Route::get('inventory/products', InventoryProduct::class)->middleware(['role:Admin|Accountant|Manager'])->name('inventory.product');
+    Route::get('inventory/details/{product_id?}', InventoryDetails::class)->middleware(['role:Admin|Accountant|Manager'])->name('inventory.details');
     //Expenses
     Route::get('expenses/category', ExpensesCategory::class)->middleware(['role:Admin|Accountant|manager'])->name('expenses.category');
 //    Route::get('expenses/list', ExpensesList::class)->name('expenses');
-    Route::get('expenses/details/{category_id?}', ExpensesList::class)->middleware(['role:Admin|Accountant|manager'])->name('expenses.details');
+    Route::get('expenses/details/{category_id?}', ExpensesList::class)->middleware(['role:Admin|Accountant|Manager'])->name('expenses.details');
     //Peoples
-    Route::get('people/customer', CustomerList::class)->middleware(['role:Admin|Accountant|manager'])->name('customer');
-    Route::get('people/supplier', SupplierList::class)->middleware(['role:Admin|Accountant|manager'])->name('supplier');
+    Route::get('people/customer', CustomerList::class)->middleware(['role:Admin|Accountant|Manager'])->name('customer');
+    Route::get('people/supplier', SupplierList::class)->middleware(['role:Admin|Accountant|Manager'])->name('supplier');
     //Sales
     Route::get('order', SalesList::class)->name('sales.list');
     Route::get('pos', SalesPos::class)->name('sales.pos');
     Route::get('sales/customer/{customer_id}', CustomerSale::class)->middleware(['role:Admin|Accountant|manager'])->name('sales.customer');
-    Route::get('sales/details/{sale_id}', SaleDetails::class)->middleware(['role:Admin|Accountant|manager'])->name('sales.details');
+    Route::get('sales/details/{sale_id}', SaleDetails::class)->middleware(['role:Admin|Accountant|Manager'])->name('sales.details');
     //Payroll
-    Route::get('payroll/employee', PayrollEmployee::class)->middleware(['role:Admin|Accountant|manager'])->name('payroll.employee');
+    Route::get('payroll/employee', PayrollEmployee::class)->middleware(['role:Admin|Accountant|Manager'])->name('payroll.employee');
     Route::get('payroll/payment', PayrollPayment::class)->middleware(['role:Admin|Accountant|manager'])->name('payroll.payment');
     Route::get('payroll/details/{emp_id?}', PayrollDetails::class)->middleware(['role:Admin|Accountant|manager'])->name('payroll.details');
-    Route::get('roles/1vs7du/{role_id?}', Permision::class)->middleware(['role:Admin|Accountant|manager'])->name('roles.1vs7du');
+    Route::get('roles/1vs7du/{role_id?}', Permision::class)->middleware(['role:Admin|Accountant|Manager'])->name('roles.1vs7du');
     //Production
-    Route::get('production/details/{material_id?}', ProductionDetails::class)->middleware(['role:Admin|Accountant|manager'])->name('production.details');
+    Route::get('production/details/{material_id?}', ProductionDetails::class)->middleware(['role:Admin|Accountant|Manager'])->name('production.details');
     Route::get('production/materials', ProductionMaterial::class)->middleware(['role:Admin|Accountant|manager'])->name('production.materials');
     //Report
     Route::get('report/profitloss', ProfitLoss::class)->middleware(['role:Admin|Accountant'])->name('report.profitloss');
@@ -71,7 +71,7 @@ Route::group(['middleware' => ['auth']], function () {
     Route::get('report/purchase', PurchaseReport::class)->middleware(['role:Admin|Accountant'])->name('report.purchase');
     Route::get('report/salary', SalaryReport::class)->middleware(['role:Admin|Accountant'])->name('report.salary');
     //Admin
-    Route::get('dashboard', Dashboard::class)->middleware(['role:Admin|Accountant|manager|Sales Man'])->name('admin.dashboard');
+    Route::get('dashboard', Dashboard::class)->middleware(['role:Admin|Accountant|Manager|Sales Man'])->name('admin.dashboard');
     Route::get('admin/users', ListUsers::class)->middleware(['role:Admin'])->name('admin.users');
     Route::get('admin/roles', RoleManagement::class)->middleware(['role:Admin'])->name('admin.roles');
     Route::get('admin/system', SystemSettings::class)->middleware(['role:Admin'])->name('admin.system');
